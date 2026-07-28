b962b1d3f14d147273e043ed0159e7012ab3f848
verta1878 <67488797+verta1878@users.noreply.github.com>
2026-07-28 19:56:23 -0400

    Delete build directory

--- build/build-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-cd "$(dirname "$0")/../src"
-fpc -O3 -Xs olms.pas && mv -f olms ../olms
-fpc -O3 -Xs config.pas && mv -f config ../config
-echo "Built: olms, config (Linux x86-64)"
